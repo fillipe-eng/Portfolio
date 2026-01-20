e655722db78414b0f7b743ed4277475ed1051738
fillipe-eng <fillipe-m1@hotmail.com>
2026-01-20 14:31:21 -0300

    Adicionando modal

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let count = 1;
 let count1 = 4;
 document.getElementById("radio1").checked = true
+let slide1 = document.getElementById("sButton1")
+let modal1 = document.getElementById("modal1")
+let eButton1 = document.getElementById("eButton1")
 
 setInterval( function(){
 nextImage1()
@@ -8,6 +11,7 @@ nextImage1()
 setInterval( function(){
 nextImage2()
 }, 3000)
+
 
 function nextImage1(){
     count++;
@@ -22,4 +26,12 @@ function nextImage2(){
         count1 = 4;
     }
     document.getElementById("radio"+count1).checked = true;
+}
+
+
+slide1.onclick = function (){
+    modal1.show()
+}
+eButton1.onclick = function (){
+    modal1.close()
 }
